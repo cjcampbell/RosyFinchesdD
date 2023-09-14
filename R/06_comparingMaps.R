9b0c25d6f6b17176abc84c3efb8b57a06d4c87dd
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2023-09-14 19:19:42 -0400

    tidying

--- R/06_comparingMaps.R
+++ R/06_comparingMaps.R
@@ -1,10 +1,13 @@
+source("~/RosyFinchesdD/.Rprofile")
 
 # Load abundance-weighted origin maps.
-maps <- list.files(file.path(wd$out, "combined_maps"), recursive = TRUE, pattern = "_cont.tif$", full.names = T) %>%
-  lapply(terra::rast) %>%
-  terra::rast() %>%
+myfiles <- list.files(file.path(wd$out, "combined_maps"), recursive = TRUE, pattern = "_cont.tif$", full.names = T)
+maps <- myfiles %>%
+  bettermc::mclapply(.,mc.cores = 3, function(x) {
+    .findProductThenNormalize( makeQuantileSurfaces(raster(x)) )
+  }) %>%
   raster::stack()
-names(maps) <- gsub("_cont", "", names(maps))
+names(maps) <- gsub("_cont.tif", "", basename(myfiles))
 
 # Load data.
 suppressWarnings(suppressMessages({ source("~/RosyFinchesdD/R/01_loadIsotopeData.R") }))
@@ -12,104 +15,86 @@ suppressWarnings(suppressMessages({ source("~/RosyFinchesdD/R/01_loadIsotopeData
 # Make simmatrix ------------------------------------------------
 # For each species:
 lapply(c("GCRF", "BLRF"), function(spp) {
-  print(paste("Working on", spp))
-  mymaps <- maps[[ unlist(mydata[ mydata$Species == spp, "ID" ]) ]]
-  simmatrix <- isocat::simmatrixMaker(
-    mymaps,
-    nClusters = FALSE,
-    csvSavePath = FALSE
-  )
-  saveRDS(simmatrix, file = file.path( wd$bin, paste0(spp, "_simmatrix.rds") ))
+  lapply(c("Claw", "Feather"), function(st) {
+
+    whichmaps <- unlist(mydata[ mydata$Species == spp & mydata$sampleType == st, "ID" ])
+    if (length(whichmaps) != 0) {
+      print(paste("Working on", spp, st))
+      mymaps <- maps[[ whichmaps ]]
+      simmatrix <- isocat::simmatrixMaker(
+        mymaps,
+        nClusters = FALSE,
+        csvSavePath = FALSE
+      )
+      saveRDS(
+        simmatrix,
+        file = file.path( wd$bin, paste(spp, st, "simmatrix.rds", sep ="_") )
+        )
+    }
+  })
 })
 
 # Perform hierarchical clustering -----------------------------------------
 # For each species, perform normal clustering based on similarity matrices.
 
 lapply(c("GCRF", "BLRF"), function(spp) {
-  simmatrix <- readRDS(file.path( wd$bin, paste0(spp, "_simmatrix.rds")))
-  simmatrix_dist <- dist( 1 - simmatrix, diag = TRUE, upper = TRUE )
-  clustered_simmatrix <- hclust( simmatrix_dist, method = "ward.D2")
-  save(clustered_simmatrix, file = file.path(wd$bin, paste0(spp, "clustered_simmatrices.Rdata") ))
+ lapply(c("Claw", "Feather"), function(st) {
+   mypath <- file.path( wd$bin, paste(spp, st, "simmatrix.rds", sep = "_"))
+   if(file.exists(mypath)) {
+    simmatrix <- readRDS(mypath)
+    simmatrix_dist <- dist( 1 - simmatrix, diag = TRUE, upper = TRUE )
+    clustered_simmatrix <- hclust( simmatrix_dist, method = "ward.D2")
+    save(clustered_simmatrix, file = file.path(wd$bin, paste(spp, st, "clustered_simmatrices.Rdata", sep= "_") ))
 
-  wss <- rep(NA, 15)
-  for (i in 1:15) wss[i] <- sum(kmeans(simmatrix_dist, i)$withinss)
-  plot(1:15, wss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
+    wss <- rep(NA, 15)
+    for (i in 1:15) wss[i] <- sum(kmeans(simmatrix_dist, i)$withinss)
+    plot(1:15, wss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
 
-  if(spp == "GCRF") { k <- 3} else {k <- 2}
-  # Plot trees.
-  plot(clustered_simmatrix, cex = 0.6)
-  rect.hclust(clustered_simmatrix, k = k, border = 2:8)
+    if(spp == "GCRF") { k <- 4} else {k <- 4}
+    # Plot trees.
+    plot(clustered_simmatrix, cex = 0.6)
+    rect.hclust(clustered_simmatrix, k = k, border = 2:8)
 
+
+    mytree <- clustered_simmatrix
+    rawCluster <- stats::cutree( mytree, k = k )
+
+    myClustDeets <- data.frame(
+      rawCluster = rawCluster, ID = mytree$labels,
+      Species = spp, sampleType = st
+      )
+
+    # Reorder clusters w/ respect to d2H vals (approximately, latitude)
+    myClustDeets <- myClustDeets %>%
+      left_join(mydata) %>%
+      group_by(rawCluster) %>%
+      dplyr::summarise(mean = mean(d2H)) %>%
+      arrange(mean) %>%
+      dplyr::mutate(cluster = row_number()) %>%
+      dplyr::select(rawCluster, cluster) %>%
+      left_join(myClustDeets, .) %>%
+      dplyr::select(-rawCluster)
+
+    saveRDS(myClustDeets, file = file.path(wd$bin, paste(spp, st, "clustDeets.rds", sep = "_")))
+   }
+ })
 })
 
-#  Prepare to cut trees into groups of individuals with common origins ----------------
+mydata_clustered <- list.files(wd$bin, pattern = "clustDeets.rds$", full.names = T) %>%
+  lapply(readRDS) %>%
+  bind_rows() %>%
+  full_join(mydata, by = c("ID", "Species", "sampleType"))
 
-lapply(c("GCRF", "BLRF"), function(spp) {
-# k-means cluster number
-wss <- rep(NA, 15)
-for (i in 1:15) wss[i] <- sum(kmeans(simmatrix_dist, i)$withinss)
-plot(1:15, wss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
-
-# Plot trees.
-plot(clustered_simmatrix, cex = 0.6)
-rect.hclust(clustered_simmatrix, k = 6, border = 2:8)
+saveRDS(mydata_clustered, file = file.path(wd$bin, "mydata_clustered.rds"))
 
 
-# # # Estimate the latitude of highest probability of origin for each surface ------
-# mydata_transformed <- readRDS( file.path(wd$bin, "mydata_transformed.rds") )
-# maps_cropped_df    <- readRDS( file.path(wd$bin, "maps_df.rds") ) %>%
-#   dplyr::filter(method == "raw")
-#
-# ave_lat <- lapply(1:nrow(mydata_transformed), function(i){
-#   ex <- mydata_transformed[i, ]
-#   u1 <- na.omit( maps_cropped_df[ maps_cropped_df$ID == ex$SampleName , ] )
-#   lats <- base::sample(size = 1000, x = u1$y, replace = TRUE, prob = u1$value)
-#   mean(lats)
-# } ) %>%
-#   unlist
-#
-# mydata_aveLat <- mydata_transformed %>% dplyr::mutate(ave_lat = ave_lat)
-#
-# saveRDS(mydata_aveLat, file = file.path(wd$bin, "mydata_aveLat.rds"))
+# Exploratory analyses ----------------------------------------------------
 
-# Manually decide how many clusters to cut into. -----------------------
-# This part requires manual input!
-my_k <- 4
+mydata_clustered %>%
+  ggplot() +
+  aes(x = Species, y = cluster) +
+  geom_jitter()
 
-# Cut trees into groups ---------------------------------------------------
-# Load data.
-mydata_aveLat <- readRDS( file.path(wd$bin, "mydata_aveLat.rds") )
-load(file.path(wd$bin, "clustered_simmatrices.Rdata"))
 
-# Apply.
 
-mytree <- clustered_simmatrix
-rawCluster <- stats::cutree( mytree, k = my_k )
-
-myClustDeets <- as.data.frame(rawCluster)
-myClustDeets$SampleName <- mytree$labels
-
-# Reorder cluster numbers with respect to descending latitude.
-myClustDeets0 <- mydata_aveLat %>%
-  right_join(myClustDeets, by = "SampleName")
-lat_key <- myClustDeets0 %>%
-  group_by(rawCluster) %>%
-  dplyr::summarise(meanLat = mean(ave_lat, na.rm = TRUE)) %>%
-  arrange(meanLat)
-lat_key$order <- 1:nrow(lat_key)
-
-save(lat_key, file = file.path( wd$bin, "recodedClusters.Rdata") )
-
-myClustDeets <- myClustDeets %>%
-  dplyr::mutate(OriginCluster = plyr::mapvalues(
-    rawCluster, lat_key$rawCluster, lat_key$order)
-  ) %>%
-  dplyr::select(SampleName, OriginCluster)
-
-myClustDeets$originColor <- viridisLite::viridis(
-  length(unique(myClustDeets$OriginCluster)))[myClustDeets$OriginCluster]
-
-mydata_clustered <- myClustDeets
-
-save(mydata_clustered, file = file.path(wd$bin, "mydata_clustered.Rdata"))
 
