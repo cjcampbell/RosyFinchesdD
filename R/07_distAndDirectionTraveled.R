b821c19ea8c0b4db23c477598a5451de5c487c17
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2023-09-14 19:18:31 -0400

    init

---
+++ R/07_distAndDirectionTraveled.R
@@ -0,0 +1,142 @@
+
+
+# distDirSummaryPlot ------------------------------------------------------
+
+
+if(!exists("distDir_ests")) distDir_ests <- fread( file.path(wd$bin, "distDir_ests.csv"))
+if(!exists("minDistDeets")) minDistDeets <- fread(file.path(wd$bin, "minDistDeets"))
+
+p_dist_BLRF <- distDir_ests %>%
+  left_join(., mydata_clustered) %>%
+  left_join(minDistDeets) %>%
+  dplyr::filter(Species == "BLRF") %>%
+  dplyr::mutate( subspecies = case_when(grepl("Hepburns", Notes) ~ "GCRF-Hep", TRUE ~ Species) ) %>%
+  dplyr::filter(sampleType == "Feather") %>%
+  group_by(Species, sampleType) %>%
+  arrange(desc(d2H)) %>%
+  dplyr::mutate(rn = row_number()) %>%
+  ggplot() +
+  geom_rect(aes(xmin = dist_25, xmax = dist_75, ymin = rn - 0.3 , ymax = rn + 0.3 , color = subspecies), fill = NA) +
+  geom_segment(aes(y = rn - 0.3 , yend = rn + 0.3 , x = dist_50, xend = dist_50, color = subspecies), linewidth = 2) +
+  geom_segment(aes(y = rn, yend = rn, x = dist_05, xend =dist_25 ,color = subspecies)) +
+  geom_segment(aes(y = rn, yend = rn, x = dist_75, xend =dist_95 ,color = subspecies)) +
+  scale_x_continuous("Distance from origin to sample site (km)", limits = c(0,NA)) +
+  scale_color_manual(values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  scale_fill_manual( values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  ylab(NULL) +
+  coord_cartesian(clip = "off") +
+  theme_bw() +
+  theme(
+    axis.text.y = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.ticks.x.top = element_line(),
+    strip.background = element_blank(),
+    strip.text = element_text(size = 15, face = "bold"),
+    legend.position = "none",
+    legend.background = element_rect(fill = NA)
+  )
+
+
+p_dist_GCRF <- distDir_ests %>%
+  left_join(., mydata_clustered) %>%
+  left_join(minDistDeets) %>%
+  dplyr::filter(Species == "GCRF") %>%
+  dplyr::mutate( subspecies = case_when(grepl("Hepburns", Notes) ~ "GCRF-Hep", TRUE ~ Species) ) %>%
+  dplyr::filter(sampleType == "Feather") %>%
+  group_by(Species, sampleType) %>%
+  arrange(desc(d2H)) %>%
+  dplyr::mutate(rn = row_number()) %>%
+  ggplot() +
+  geom_rect(aes(xmin = dist_25, xmax = dist_75, ymin = rn - 0.3 , ymax = rn + 0.3 , color = subspecies), fill = NA) +
+  geom_segment(aes(y = rn - 0.3 , yend = rn + 0.3 , x = dist_50, xend = dist_50, color = subspecies), linewidth = 2) +
+  geom_segment(aes(y = rn, yend = rn, x = dist_05, xend =dist_25 ,color = subspecies)) +
+  geom_segment(aes(y = rn, yend = rn, x = dist_75, xend =dist_95 ,color = subspecies)) +
+  scale_x_continuous("Distance from origin to sample site (km)", limits = c(0,NA)) +
+  scale_color_manual(values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  scale_fill_manual( values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  ylab(NULL) +
+  coord_cartesian(clip = "off") +
+  theme_bw() +
+  theme(
+    axis.text.y = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.ticks.x.top = element_line(),
+    strip.background = element_blank(),
+    strip.text = element_text(size = 15, face = "bold"),
+    legend.position = "none",
+    legend.background = element_rect(fill = NA)
+  )
+
+
+p_direction_BLRF <- minDistDeets %>%
+  dplyr::mutate(
+    subspecies = case_when(grepl("Hepburns", Notes) ~ "GCRF-Hep", TRUE ~ Species)
+  ) %>%
+  dplyr::filter(Species == "BLRF", sampleType == "Feather") %>%
+  ggplot() +
+  geom_histogram(aes(theta_from_site, fill = subspecies), binwidth = 45/2) +
+  coord_polar(theta = "x", start = pi) +
+  geom_star(aes(x=0,y=0), size = 2, fill = "white") +
+  scale_x_continuous(
+    name = NULL,
+    breaks = seq(-180,179,90),
+    limits = c(-180,180),
+    expand = c(0,0),
+    labels = c("S", "W", "N", "E")
+  ) +
+  scale_fill_manual( values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  theme_bw() +
+  theme(
+    axis.title = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.ticks.x.top = element_line(),
+    strip.background = element_blank(),
+    strip.text = element_text(size = 10, face = "bold"),
+    legend.background = element_rect(fill = NA),
+    legend.position = "none"
+  )
+
+p_direction_GCRF <- minDistDeets %>%
+  dplyr::mutate(
+    subspecies = case_when(grepl("Hepburns", Notes) ~ "GCRF-Hep", TRUE ~ Species)
+  ) %>%
+  dplyr::filter(Species == "GCRF", sampleType == "Feather") %>%
+  ggplot() +
+  geom_histogram(aes(theta_from_site, fill = subspecies), binwidth = 45/2) +
+  coord_polar(theta = "x", start = pi) +
+  scale_x_continuous(
+    name = NULL,
+    breaks = seq(-180,179,90),
+    limits = c(-180,180),
+    expand = c(0,0),
+    labels = c("S", "W", "N", "E")
+  ) +
+  geom_star(aes(x=0,y=0), size = 2, fill = "white") +
+  scale_fill_manual( values = c("BLRF" = "grey20", "GCRF" = "grey30", "GCRF-Hep" = "darkgoldenrod"))+
+  theme_bw() +
+  theme(
+    axis.title = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.ticks.x.top = element_line(),
+    strip.background = element_blank(),
+    strip.text = element_text(size = 10, face = "bold"),
+    legend.background = element_rect(fill = NA),
+    legend.position = c(0.10,0.1),
+    legend.title = element_blank()
+  )
+
+ggarrange(p_dist_BLRF,p_direction_BLRF, p_dist_GCRF,p_direction_GCRF, ncol = 2, nrow = 2, labels = LETTERS) %>%
+  ggsave(., filename = file.path(wd$figs, "distDIR-feathers_species.png"), width = 6, height = 6)
+
